b5c035d1f163bdcd6d308e65bf0e3db3b0ef891e
harsh <narlon@163.com>
2018-10-10 15:14:59 +0800

    add the structure

---
+++ Script/compileCfgDll.bat
@@ -0,0 +1,20 @@
+cd ..\Config
+
+IF NOT EXIST ".\ConfigData" MD ".\ConfigData"
+
+ExcelToCsv.exe
+cd .\Config
+csc /target:library /r:../NarlonLib.dll /r:../BaseType.dll /out:../ConfigData.dll *.cs
+if not "%ERRORLEVEL%" == "0" GOTO :ERROR
+
+cd ..
+copy "./ConfigData.dll" "../Client/Assets/plugins/"
+@copy "./ConfigData.dll" "../Tool/bin/"
+
+GOTO :END
+
+:ERROR
+@ECHO COMPILATION FAILED
+PAUSE
+
+:END
