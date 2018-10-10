dac3157b87aafc315a7bc7a24914999294e603e7
harsh <narlon@163.com>
2018-10-10 16:08:15 +0800

    add two land types

---
+++ Client/Assets/Script/DGToK/GFS/WaterFlow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterFlow : MonoBehaviour
+{
+
+    public float m_SpeedU = 0.1f;
+    public float m_SpeedV = -0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newOffsetU = Time.time * m_SpeedU;
+        float newOffsetV = Time.time * m_SpeedV;
+
+        var renderComp = this.GetComponent<Renderer>();
+        if (renderComp != null)
+        {
+            renderComp.material.mainTextureOffset = new Vector2(newOffsetU, newOffsetV);
+        }
+    }
+}
---
+++ Client/Assets/Script/DGToK/StickInit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NarlonLib.Math;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class StickInit : MonoBehaviour
+{
+    public GameObject GreenStick;
+    public GameObject BlueStick;
+
+    // Use this for initialization
+    void Start () {
+	    for (int i = -10; i < 10; i++)
+        {
+            for (int j = -10; j < 10; j++)
+            {
+                GameObject tickItem;
+                if (MathTool.IsRandomInRange01(0.15f))
+                {
+                    tickItem = Instantiate(BlueStick, gameObject.transform);
+                    tickItem.transform.localScale = new Vector3(3, 1, 3);
+                }
+                else
+                {
+                    tickItem = Instantiate(GreenStick, gameObject.transform);
+                    tickItem.transform.localScale = new Vector3(3, MathTool.GetRandom(7), 3); //1-7的高度
+                }
+                tickItem.transform.position = new Vector3(i*3,0.5f,j*3);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
